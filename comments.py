e8127c5fc6813ab7c02f79c87a2546670404e0af
swwapnilshitole <47440897+swwapnilshitole@users.noreply.github.com>
2020-02-23 13:11:08 +0530

    Day 1

---
+++ comments.py
@@ -0,0 +1,10 @@
+name='swapnil' #hey
+#this is name of user.
+age=22
+'''
+bio="birthday is on 19th oct"
+bio="birthday is on 19th oct"
+bio="birthday is on 19th oct"
+bio="birthday is on 19th"
+'''
+print("hello my name is",name,"my age is",age)
---
+++ datatypes.py
@@ -0,0 +1,9 @@
+name='swapnil'
+age=22
+some_lists = ['maths','padc','physics','chem']
+student = {
+    'name':'swapnil',
+    'age':22,
+    'subjects': some_lists
+} 
+print(student['subjects'][2])
